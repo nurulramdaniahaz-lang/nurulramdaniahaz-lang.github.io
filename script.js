0f392b07b2dcfe0acfe1a9c430e75b4843453ab5
nurulramdaniahaz-lang <nurulramdaniahaz@gmail.com>
2025-12-09 01:59:05 +0700

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Website dari HP berjalan!");
+function createSakura() {
+    const sakura = document.createElement("img");
+    sakura.src = "https://cdn-icons-png.flaticon.com/512/7656/7656408.png";
+    sakura.classList.add("sakura");
+
+    sakura.style.left = Math.random() * window.innerWidth + "px";
+
+    document.body.appendChild(sakura);
+
+    setTimeout(() => {
+        sakura.remove();
+    }, 7000);
+}
+
+setInterval(createSakura, 400);
